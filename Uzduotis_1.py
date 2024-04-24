c48957a444e499f4be915e6d55fe3c69aaa6e6d6
RasaDovyd <dovydaitiene.rasa@gmail.com>
2024-04-24 13:24:38 +0300

    ok

--- Uzduotis_1.py
+++ Uzduotis_1.py
@@ -4,16 +4,18 @@
 # pateikus žodį "alus", grąžintų "sula"
 # parašykite keletą testų šiai funkcijai
 
-
 def apverstas_tekstas(a):
-    return ' '.join(word[::-1] for word in a.split())
+    return a[::-1]
+
 
 # 2 užduotis
 # parašykite funkciją, kuri kaip argumentą priima sąrašą ir grąžina to sąrašo narių sumą
 # parašykite keletą testų šiai funkcijai
 
-def saraso_ilgis(sarasas:list):
-    return sum(sarasas)
+def saraso_suma(sarasas:list):
+    suma = sum(sarasas)
+    return suma
+
 
 # 3 užduotis
 # parašykite funkciją, kuri kaip argumentą priima sąrašą ir grąžina jame esančius TEIGIAMUS skaičius
@@ -26,12 +28,37 @@ def teigiami_skaiciai(sarasas:list):
             naujas_sarasas.append(skaicius)
     return naujas_sarasas
 
+
 # 4 užduotis
 # Parašykite funkciją, kuri priima dvi sąrašų rūšiavimo funkcijas: vieną didėjančiai rūšiavimui, kitą mažėjančiai rūšiavimui, ir sąrašą skaičių. 
 # Funkcija turi grąžinti rūšiuotą sąrašą pagal pateiktas rūšiavimo funkcijas.
 # parašykite keletą testų šiai funkcijai
 
-def saraso_rusiavimas(sarasas:list):
-    return sarasas
-    return sarasas.sort(reverse=False)
-    return sarasas.sort(reverse=True)
+def saraso_rikiavimas(f:str,sarasas:list)-> list: # f- funkcija
+    def rikiuoti_mazejanciai(sarasas):
+      return sorted(sarasas,reverse=False)  
+
+    def rikiuoti_didejanciai(sarasas):
+      return sorted(sarasas) 
+
+    if f == 'didejant':
+        return rikiuoti_didejanciai(sarasas)
+    else:
+        return rikiuoti_mazejanciai(sarasas)
+
+print(saraso_rikiavimas('didejant', [1,5,2]))
+
+
+# funkcija pteikiam kaip argumenta
+
+def apskaiciuoti(funkcija, skaicius1, skaicius2):
+    return funkcija(skaicius1, skaicius2)
+
+def sudetis(num1,num2):
+    return num1+num2
+
+def  daugyba(num1,num2):
+    return num1*num2
+
+print(apskaiciuoti(sudetis(),2,5))
+print(sudetis)
--- test_uzduotis_1.py
+++ test_uzduotis_1.py
@@ -1,20 +1,34 @@
-import Uzduotys_1
+import Uzduotis_1
 
 def test_apverstas_tekstas():
     # return ' '.join(word[::-1] for word in a.split())
-    assert Uzduotys_1.apverstas_tekstas('alus') == 'sula'
+    assert Uzduotis_1.apverstas_tekstas('alus') == 'sula'
+    assert Uzduotis_1.apverstas_tekstas('2563') == '3652'
+    assert Uzduotis_1.apverstas_tekstas('s a l t a') == 'a t l a s' # cia net nezinau kokio rezultato tiketis
 
-# def test_saraso_ilgis(sarasas:list):
-    # return sum(sarasas)
+def test_saraso_suma(sarasas):
+    sarasas = [2,5,7]
+    assert Uzduotis_1.saraso_suma(sarasas) == 14
 
-# def test_teigiami_skaiciai(sarasas:list):
+def test_saraso_suma_raides(sarasas):
+    sarasas = ['s','g']
+    assert Uzduotis_1.saraso_suma(sarasas) == Error # nezinau kaip ivardinti tiketina rezultata
+
+def test_saraso_suma_raides(sarasas):
+    sarasas = [5,'s',3] # nezinau ar reikia toki scenariju testtuoti
+    assert Uzduotis_1.saraso_suma(sarasas) == Error 
+
+def test_teigiami_skaiciai(sarasas:list):
     # naujas_sarasas = []
     # for skaicius in sarasas:
     #     if skaicius> 0:
     #         naujas_sarasas.append(skaicius)
     # return naujas_sarasas
+    sarasas = [-5,5,2,-6,-8,4]
+    assert Uzduotis_1.teigiami_skaiciai(sarasas) == [5,2,4]
+
+def test_teigiami_skaiciai_eror(sarasas:list):
+    sarasas = [-5,5,a,-6,-8,4]
+    assert Uzduotis_1.teigiami_skaiciai(sarasas) == error  
 
 # def test_saraso_rusiavimas(sarasas:list):
-    # return sarasas
-    # return sarasas.sort(reverse=False)
-    # return sarasas.sort(reverse=True)
